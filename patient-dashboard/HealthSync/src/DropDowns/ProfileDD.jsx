c99b3fb26c31893895f934e335b8820aa5f70f3e
Saurav Chaudhary <chaudharygroup2015@gmail.com>
2024-03-11 15:19:13 +0530

    Changed the structure

--- patient-dashboard/HealthSync/src/DropDowns/ProfileDD.jsx
+++ patient-dashboard/HealthSync/src/DropDowns/ProfileDD.jsx
@@ -4,7 +4,11 @@ import styles from "../All-CSSs/ProfileDD.module.css";
 const ProfileDD = () => {
    return (
       <div className={styles.profileDD}>
-         <ProfileDDLI text='View Profile'></ProfileDDLI>
+         <ProfileDDLI text='View Profile' imgsrc=''></ProfileDDLI>
+         <ProfileDDLI text='Edit Profile' imgsrc=''></ProfileDDLI>
+         <ProfileDDLI text='Settings' imgsrc=''></ProfileDDLI>
+         <ProfileDDLI text='Help' imgsrc=''></ProfileDDLI>
+         <ProfileDDLI text='Logout' imgsrc=''></ProfileDDLI>
       </div>
    );
 };
